ec812f403f72f7e79f38b40ee5c972b3a7e7aaf1
Marjanno Evert <marjanno.evert@tktk.ee>
2024-08-26 22:18:40 +0300

    week 3

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter a number");
+int usernum = Convert.ToInt32(Console.ReadLine());
+
+// kas kasutaja sisestatud arv on paaris või paaritu
+
+int result = usernum % 2;
+
+if (result != 0)
+{
+    Console.WriteLine("User number is odd");
+}
+else
+{
+    Console.WriteLine("User number is even");
+}
---
+++ Week3IfAndSwitch/Program.cs
@@ -0,0 +1,8 @@
+﻿//modulus - %
+
+int a = 19;
+int b = 7;
+
+int c = a % b;  //+/-/*-/
+
+Console.WriteLine(c);
